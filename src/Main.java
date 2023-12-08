078bb556309a3eb67555772da85e15394965ab08
jensen.rs.turner@gmail.com <jensen.rs.turner@gmail.com>
2023-12-08 16:02:55 -0500

    final submitted version of Main.java

--- src/Main.java
+++ src/Main.java
@@ -16,20 +16,24 @@ public class Main {
         this.stdin = stdin;
     }
 
-    @FunctionalInterface interface MenuMethod {
+    @FunctionalInterface
+    interface MenuMethod {
         void run() throws SQLException;
     }
 
     static class MenuEntry {
         private final String description;
         private final MenuMethod code;
+
         MenuEntry(String description, MenuMethod code) {
             this.description = description;
             this.code = code;
         }
+
         String description() {
             return this.description;
         }
+
         MenuMethod code() {
             return this.code;
         }
@@ -40,6 +44,8 @@ public class Main {
             new MenuEntry("Create Reservation - Existing Guest", this::createReservationExistingGuest),
             new MenuEntry("Check In Reservation", this::checkIn),
             new MenuEntry("Check Out Reservation", this::checkOut),
+            new MenuEntry("Change Reservation", this::changeReservation),
+            new MenuEntry("Delete Reservation", this::deleteReservation),
             new MenuEntry("Assign Room for Maintenance", this::assignRoomForMaintenance),
             new MenuEntry("Log Maintenance Completion", this::logMaintenanceCompletion),
             new MenuEntry("Remove Maintenance Assignment", this::removeMaintenanceAssignment),
@@ -95,6 +101,8 @@ public class Main {
     }
 
     void createReservationInternal(int guestId) throws SQLException {
+        String roomId = searchRoom();
+
         System.out.print("Enter start date (e.g. 6/1/23): ");
         Date startDate;
         try {
@@ -115,15 +123,20 @@ public class Main {
             return;
         }
 
-        CallableStatement createReservationStatement = connection.prepareCall("{call dbo.createReservation(?,?,?,?)}");
-        createReservationStatement.setDate(1, startDate);
-        createReservationStatement.setDate(2, endDate);
-        createReservationStatement.setInt(3, guestId);
-        createReservationStatement.registerOutParameter(4, Types.INTEGER);
-        createReservationStatement.execute();
+        try {
+            CallableStatement createReservationStatement = connection.prepareCall("{call dbo.createReservation(?,?,?,?,?)}");
+            createReservationStatement.setDate(1, startDate);
+            createReservationStatement.setDate(2, endDate);
+            createReservationStatement.setInt(3, guestId);
+            createReservationStatement.setNString(4, roomId);
+            createReservationStatement.registerOutParameter(5, Types.INTEGER);
+            createReservationStatement.execute();
 
-        connection.commit();
-        System.out.println("Reservation created");
+            connection.commit();
+            System.out.println("Reservation created");
+        } catch (SQLException e) {
+            System.out.println("Failed to create reservation: " + e.getMessage());
+        }
     }
 
     int searchReservationByGuestName(String status) throws SQLException {
@@ -135,7 +148,7 @@ public class Main {
         searchReservationsStatement.setString(2, status);
         ResultSet results = searchReservationsStatement.executeQuery();
 
-        System.out.println("option | guest name | start date | end date");
+        System.out.println("option | guest name | start date | end date | room number");
         List<Integer> reservationIdsInOrder = new ArrayList<>();
         int i = 0;
         while (results.next()) {
@@ -145,7 +158,8 @@ public class Main {
             String guestName = results.getNString("name");
             String startDate = dateFormatter.format(results.getDate("start_date"));
             String endDate = dateFormatter.format(results.getDate("end_date"));
-            System.out.println((i + 1) + " | " + guestName + " | " + startDate + " | " + endDate);
+            String roomNumber = results.getNString("room_id");
+            System.out.println((i + 1) + " | " + guestName + " | " + startDate + " | " + endDate + " | " + roomNumber);
 
             i++;
         }
@@ -154,6 +168,32 @@ public class Main {
         int chosenIndex = stdin.nextInt() - 1;
         stdin.nextLine();
         return reservationIdsInOrder.get(chosenIndex);
+    }
+
+    String searchRoom() throws SQLException {
+        System.out.print("Enter part or all of room number to search rooms (blank to show all): ");
+        String searchRoomName = stdin.nextLine();
+
+        CallableStatement searchRoomsStatement = connection.prepareCall("{call dbo.searchRooms(?)}");
+        searchRoomsStatement.setString(1, searchRoomName);
+        ResultSet results = searchRoomsStatement.executeQuery();
+
+        System.out.println("option | room number | location");
+        List<String> roomIdsInOrder = new ArrayList<>();
+        int i = 0;
+        while (results.next()) {
+            String roomNumber = results.getNString("id");
+            roomIdsInOrder.add(roomNumber);
+
+            String locationAddress = results.getNString("address");
+            System.out.println((i + 1) + " | " + roomNumber + " | " + locationAddress);
+
+            i++;
+        }
+        System.out.print("Enter selection: ");
+        int chosenIndex = stdin.nextInt() - 1;
+        stdin.nextLine();
+        return roomIdsInOrder.get(chosenIndex);
     }
 
     void checkIn() throws SQLException {
@@ -210,38 +250,16 @@ public class Main {
             System.out.println("invalid date format");
             return;
         }
-        System.out.print("Enter new room id: ")
-        String roomId = stdin.nextLine();
 
-        if(cheackRoomReservation(roomId, startDate, endDate)){
-            CallableStatement changeReservationStatement = connection.prepareCall("{call dbo.deleteReservation(?)}");
-            changeReservationStatement.setInt(1, reservationId);
-            createReservationStatement.setDate(2, startDate);
-            createReservationStatement.setDate(3, endDate);
-            createReservationStatement.setString(4, roomId);
-            changeReservationStatement.execute();
-            connection.commit();
-            System.out.println("Reservation changed to " + startDate + " to " + endDate + " in room " + roomId);
-        } else {
-            System.out.println("Room " + roomId + "cannot be booked during for that time please try a different room or different dates.");
-        }
-    }
+        CallableStatement updateReservationStatement = connection.prepareCall("{call dbo.changeReservation(?,?,?)}");
+        updateReservationStatement.setInt(1, reservationId);
+        updateReservationStatement.setDate(2, startDate);
+        updateReservationStatement.setDate(3, endDate);
+        updateReservationStatement.execute();
 
-    boolean checkRoomReservation(String roomId, Date start_date, Date end_date) throws SQLException{
-        CallableStatement roomSearchStatement = conneciton.prepareCall("{call dbo.searchReservationByRoomId(?)}");
-        roomSearchStatement.setString(1, roomId);
-        ResultSet results = searchReservationsStatement.executeQuery();
-        int i = 0;
-        while (results.next()) {
-            Date listStartDate = dateFormatter.format(results.getDate("start_date"));
-            Date listEndDate = dateFormatter.format(results.getDate("end_date"));
-            
-            if((start_date.before(listEndDate) AND start_date.after(listStartDate)) OR (end_date.before(listEndDate) AND end_date.after(listStartDate)))
-                return false;
+        connection.commit();
+        System.out.println("Reservation updated");
 
-            i++;
-        }
-        return true;
     }
 
     void updateStaffRole() throws SQLException {
@@ -251,7 +269,14 @@ public class Main {
         System.out.print("Enter new role for the staff: ");
         String newRole = stdin.nextLine();
 
-        DatabaseConnection.updateStaffRole(staffId, newRole);
+        String sql = "{call UpdateStaffRole(?, ?)}";
+        CallableStatement cstmt = connection.prepareCall(sql);
+
+        cstmt.setInt(1, staffId);
+        cstmt.setString(2, newRole);
+
+        cstmt.executeUpdate();
+        connection.commit(); // Commit the transaction after successful execution
         System.out.println("Staff role updated.");
     }
 
@@ -259,10 +284,18 @@ public class Main {
         try {
             System.out.print("Enter Room ID to get price: ");
             String roomId = stdin.nextLine();
-    
-            Long price = DatabaseConnection.getRoomPrice(roomId);
-            if (price != null) {
-                System.out.println("Price for room " + roomId + " is: " + price);
+
+            String sql = "{call GetRoomPrice(?, ?)}";
+            CallableStatement cstmt = connection.prepareCall(sql);
+
+            cstmt.setString(1, roomId);
+            cstmt.registerOutParameter(2, Types.BIGINT);
+
+            cstmt.execute();
+            Long roomPrice = cstmt.getLong(2);
+
+            if (roomPrice != null) {
+                System.out.println("Price for room " + roomId + " is: " + roomPrice);
             } else {
                 System.out.println("Room not found or price unavailable.");
             }
@@ -271,16 +304,23 @@ public class Main {
         } catch (Exception e) {
             System.out.println("An unexpected error occurred: " + e.getMessage());
         }
-    }    
+    }
 
     void removeMaintenanceAssignment() throws SQLException {
         System.out.print("Enter Room ID to remove maintenance assignment: ");
         String roomId = stdin.nextLine();
-    
+
         System.out.print("Enter Staff ID for the assignment: ");
         int staffId = Integer.parseInt(stdin.nextLine());
-    
-        DatabaseConnection.removeMaintenanceAssignment(roomId, staffId);
+
+        String sql = "{call RemoveMaintenanceAssignment(?, ?)}";
+        CallableStatement cstmt = connection.prepareCall(sql);
+
+        cstmt.setString(1, roomId);
+        cstmt.setInt(2, staffId);
+
+        cstmt.executeUpdate();
+        connection.commit();
         System.out.println("Maintenance assignment removed.");
     }
 
@@ -291,7 +331,14 @@ public class Main {
         System.out.print("Enter Staff ID: ");
         int staffId = Integer.parseInt(stdin.nextLine());
 
-        DatabaseConnection.assignRoomForMaintenance(roomId, staffId);
+        String sql = "{call AssignRoomForMaintenance(?, ?)}";
+        CallableStatement cstmt = connection.prepareCall(sql);
+
+        cstmt.setString(1, roomId);
+        cstmt.setInt(2, staffId);
+
+        cstmt.executeUpdate();
+        connection.commit();
         System.out.println("Room assigned for maintenance.");
     }
 
@@ -308,7 +355,15 @@ public class Main {
         System.out.print("Is the room in good condition? (true/false): ");
         boolean inGoodCondition = Boolean.parseBoolean(stdin.nextLine());
 
-        DatabaseConnection.logMaintenanceCompletion(roomId, staffId, maintenanceDate, inGoodCondition);
+        String sql = "{call LogMaintenance(?, ?, ?, ?)}";
+        CallableStatement cstmt = connection.prepareCall(sql);
+        cstmt.setString(1, roomId);
+        cstmt.setInt(2, staffId);
+        cstmt.setDate(3, maintenanceDate);
+        cstmt.setBoolean(4, inGoodCondition);
+
+        cstmt.executeUpdate();
+        connection.commit();
         System.out.println("Maintenance completion logged.");
     }
 
@@ -334,7 +389,7 @@ public class Main {
 
     public static void main(String[] args) {
         String connectionUrl =
-                "jdbc:sqlserver://cxp-sql-02\\jrt108;"
+                "jdbc:sqlserver://10.16.0.75\\jrt108;"
                         + "database=Hotel;"
                         + "user=dbuser;"
                         + "password=csds341143sdsc;"
@@ -342,13 +397,16 @@ public class Main {
                         + "trustServerCertificate=true;"
                         + "loginTimeout=15;";
 
-        try (Connection connection = DriverManager.getConnection(connectionUrl))
-        {
+        try (Connection connection = DriverManager.getConnection(connectionUrl)) {
             connection.setAutoCommit(false);
             Main m = new Main(connection, new Scanner(System.in));
             System.out.println("Welcome to the Hotel Management System");
             while (true) {
-                m.doMainMenu();
+                try {
+                    m.doMainMenu();
+                } catch (SQLException e) {
+                    System.out.println("Operation failed: " + e.getMessage());
+                }
             }
         }
         // Handle any errors that may have occurred.
